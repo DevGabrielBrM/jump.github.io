c12188a008a6bebcacdb34840b8d6a2d25ebf5ab
DevGabrielBrM <166339851+DevGabrielBrM@users.noreply.github.com>
2024-04-08 19:02:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioposition}px` ;
 
-        mario.src ='./jump.github.io/game-over.png'
+        mario.src ='https://github.com/DevGabrielB/jump.github.io/game-over.png'
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
 
