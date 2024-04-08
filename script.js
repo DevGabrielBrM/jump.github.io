e5e489b38844d35c19c6c3257dac884bdb4ce3b5
DevGabrielBrM <166339851+DevGabrielBrM@users.noreply.github.com>
2024-04-08 20:39:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -50,9 +50,9 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src ='game-over.png'
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
+        mario.src ='game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
 
         clearInterval(loop);
     }
